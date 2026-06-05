5299d7e192423b7541e4e0d05b2cf86aded68c8b
Κωνσταντινος Ματτας <109415079+Malakotadin@users.noreply.github.com>
2026-06-05 13:40:21 +0300

    Add files via upload

--- 09c_mvc_example_with_db_login_sessions_jsp_bcrypt.zip_expanded/09c_mvc-example_with_db_login_session_jsp_bcrypt/src/main/java/model/Users.java
+++ 09c_mvc_example_with_db_login_sessions_jsp_bcrypt.zip_expanded/09c_mvc-example_with_db_login_session_jsp_bcrypt/src/main/java/model/Users.java
@@ -6,6 +6,7 @@ public class Users {
 	private String name;
 	private String surname;
 	private String role;
+	private String department;
 	//Declaring UserCounter as static in order to be a class attribute and not an instance attribute
 	public static int UserCounter=0;
 
@@ -13,6 +14,14 @@ public class Users {
 	public Users(){
 		//Count the times that a user is created by class Users or a subclass (Client,Seller,Admin)
 		UserCounter+=1;
+	}
+	public Users(String username2,String name2,String  department2){
+		setUsername(username2);
+		setName(name2);
+		setDepartment(department2);
+	}
+	public void setDepartment(String department) {
+		this.department = department;
 	}
 	public void setUsername(String username) {
 		this.username=username;
@@ -45,6 +54,9 @@ public class Users {
 
 	public String getRole() {
 		return role;
+	}
+	public String getDepartment() {
+		return department;
 	}
 
 
