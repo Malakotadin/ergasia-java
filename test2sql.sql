353fd0a20329c5cd4d84ee0b9d1174b35861d8f0
bagells <vtiliakos05@gmail.com>
2026-06-16 13:55:44 +0300

    Add files via upload

---
+++ test2sql.sql
@@ -0,0 +1,39 @@
+BEGIN;
+create table if not exists grades(
+ grade_id numeric,
+ primary key (grade_id)
+);
+create table if not exists courses(
+  course_id numeric,
+  course_name "char",
+  primary key (course_id)
+);
+create table if not exists student(
+id numeric,
+    name "char",
+    surename "char",
+    courses numeric,
+    grades"numeric",
+    primary key (id),
+foreign key (courses) REFERENCES courses(course_id),
+foreign key (grades) references grade(grade_id)
+);
+create table if not exists proffesor(
+id numeric,
+   name "char",
+    surename "char",
+    courses numeric,
+    primary key (id),
+    foreign key (courses) REFERENCES courses(course_id)
+
+);
+create table if not exists secratary(
+id numeric,
+name "char",
+surename "char",
+courses numeric,
+    primary key (id),
+foreign key (courses) REFERENCES courses(course_id)
+);
+
+END;
