2b18a128f037d49bb2c5949f0c6bcc4d04222a69
bagells <vtiliakos05@gmail.com>
2026-06-20 18:16:19 +0300

    Add files via upload

---
+++ part3.sql
@@ -0,0 +1,48 @@
+   BEGIN;
+    create table if not exists grades(
+                                         grade_id numeric,
+                                         grade_number numeric,
+                                         primary key (grade_id)
+    );
+    create table if not exists student(
+                                          id numeric,
+                                          name varchar(30),
+                                          surename varchar(30),
+                                          courses numeric,
+                                          grades"numeric",
+                                          password varchar(30),
+                                          department varchar(30),
+                                          primary key (id),
+                                          foreign key (grades) references grades(grade_id)
+    );
+    create table if not exists courses(
+                                          course_id numeric,
+                                          course_name varchar(30),
+                                           s_id numeric,
+                                          primary key (course_id),
+                                         foreign key (s_id) references  student(id)
+    );
+
+    create table if not exists proffesor(
+                                            id numeric,
+                                            name varchar(30),
+                                            surename varchar(30),
+                                            courses numeric,
+                                            password varchar(30),
+                                            department varchar(30),
+                                            primary key (id),
+                                            foreign key (courses) REFERENCES courses(course_id)
+
+    );
+    create table if not exists secratary(
+                                            id numeric,
+                                            name varchar(30),
+                                            surename varchar(30),
+                                            courses numeric,
+                                            password varchar(30),
+                                            department varchar(30),
+                                            primary key (id),
+                                            foreign key (courses) REFERENCES courses(course_id)
+    );
+
+    END;
